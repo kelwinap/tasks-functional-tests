e31d073bba11773d7d7b895205ee9a6ca355f4fc
kelwinap <kelwinarthur8@gmail.com>
2021-04-28 16:27:58 -0300

    Health check test

---
+++ cypress/integration/examples/healthcheck.spec.js
@@ -0,0 +1,14 @@
+describe("Health check", () => {
+  beforeEach(() => {
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false;
+    });
+  });
+
+  it("should see title", () => {
+    cy.visit("http://192.168.0.2:9999/tasks/tasks/");
+    cy.get("h1").should("contain", "Tasks");
+  });
+});
